60122abafb5771b0dbf62a054a5d6a57d1bebc2c
sarahrosecooper <sarahrosecooperx@gmail.com>
2021-03-18 02:36:11 -0400

    (build) Setting up basic component structure

--- sarah-rose-port/src/App.js
+++ sarah-rose-port/src/App.js
@@ -1,9 +1,14 @@
+import React from "react";
 import "./App.css";
+import Navigation from "./components/Navigation";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
       <p>home</p>
+      <Navigation />
     </div>
   );
 }
